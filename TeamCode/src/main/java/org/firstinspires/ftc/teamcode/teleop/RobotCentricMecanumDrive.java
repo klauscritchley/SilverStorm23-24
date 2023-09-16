c6da81a2457b1ac0b23ba67b3239b719b3b5a33c
Lolbit757575 <cosmicaura3@gmail.com>
2023-09-16 16:15:25 -0500

    Minor Edit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotCentricMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotCentricMecanumDrive.java
@@ -1,18 +1,13 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.teleop;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.opencv.core.Mat;
-
-@TeleOp(name="Mecanum Test", group = "Tests")
-public class TeleopTest extends LinearOpMode {
+@TeleOp(name="Robot Centric Mecanum", group = "Tests")
+public class RobotCentricMecanumDrive extends LinearOpMode {
 
     private  ElapsedTime runtime = new ElapsedTime();
     private DcMotor rightFrontDrive;
